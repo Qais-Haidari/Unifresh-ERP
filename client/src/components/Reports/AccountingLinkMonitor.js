e07d248d9cb37d60221f02dcd54d7888068d7f1c
qais haidari <qaishaidari555@gmail.com>
2024-09-13 14:51:28 +0930

    asdfasdf

--- client/src/components/Reports/AccountingLinkMonitor.js
+++ client/src/components/Reports/AccountingLinkMonitor.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
+import { URL } from '../../Utils/URL';
 
 export default function AccountingLinkMonitor() {
     const [JL, setJL] = useState("");
